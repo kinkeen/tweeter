a8c92580825663ae8aabf3563f2e167f7f49871a
kineret.braslavsky <kineret.braslavsky@gmail.com>
2021-01-28 03:39:37 +0000

    add commants and orgenize the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,64 +1,64 @@
 
-    const renderTweets = function(tweets) {
-      for (const tweet of tweets) {
-        const $htmltweet = createTweetElement(tweet);
-        $('#tweets-container').append($htmltweet)
-      }
-    };
+//looping through the tweets in the server and creating HTML element for each tweet
+const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+    const $htmltweet = createTweetElement(tweet);
+    $('#tweets-container').append($htmltweet)
+    }
+};
   
-    const createTweetElement = function(tweetData) {
-        // using jquery to create a DOM object
-        const tweetArticle = `<article class="tweet"> 
-         <header>
-           <div class="tweet-icon">
-           <img class="icon" src=${tweetData['user'].avatars}>
-           <p>${tweetData['user'].name}</p>
-           </div>
-           <div><p>${tweetData['user'].handle}</p></div>
-         </header>
-         <div class="tweet-body">
-           <p>${tweetData['content'].text}</p>
-        </div>
-        <hr class="line">
-         <footer class="footer">
-         
-          <p>${tweetData['created_at']}</p>
-           <div>
-             <i class="fa fa-flag"></i>
-             <i class="fa fa-retweet"></i>
-             <i class="fa fa-heart"></i>
-          </div>
-         </footer>
-       </article>`;
-     
-       return tweetArticle;
-      };
-  
-  
-      $(function() {
-        const $form = $("#newtweet")
-        $form.submit(function(event) {
-          event.preventDefault();
-          const tweetcontent = $(this).children("#tweet-text")
-          console.log(tweetcontent)
-          $.ajax({
-            method: 'POST',
-            url: "/tweets",
-            data: $(this).serialize()
-          }).then(res => {
-            console.log(res)
-          })
-      
-        });
+//creating html element for a tweet
+const createTweetElement = function(tweetData) {
+  // using jquery to create a DOM object
+  const tweetArticle = `
+        <article class="tweet"> 
+            <header>
+                <div class="tweet-icon">
+                    <img class="icon" src=${tweetData['user'].avatars}>
+                    <p>${tweetData['user'].name}</p>
+                </div>
+                <div><p>${tweetData['user'].handle}</p></div>
+            </header>
+            <div class="tweet-body">
+                <p>${tweetData['content'].text}</p>
+            </div>
+            <hr class="line">
+            <footer class="footer">
+                <p>${tweetData['created_at']}</p>
+                <div>
+                    <i class="fa fa-flag"></i> <i class="fa fa-retweet"></i> <i class="fa fa-heart"></i>
+                </div>
+            </footer>
+        </article>`;
+    
+    return tweetArticle;
+};
 
-         // function to load tweets from the server
-  const loadTweets = function() {
+  // AJAX POST for submit button
+$(function() {
+    const $form = $("#newtweet")
+    $form.submit(function(event) {
+    event.preventDefault();
+    const tweetcontent = $(this).children("#tweet-text")
+    console.log(tweetcontent)
+    $.ajax({
+        method: 'POST',
+        url: "/tweets",
+        data: $(this).serialize()
+    }).then(res => {
+        console.log(res)
+    })
+    
+});
+
+// function to load tweets from the server
+const loadTweets = function() {
     $.getJSON("/tweets", function(data) {
-      console.log('success',data)
+        console.log('success',data)
     })
     .then(function (tweetdata) {
-      renderTweets(tweetdata)
+        renderTweets(tweetdata)
     })
   };
   loadTweets()
- });
+});
